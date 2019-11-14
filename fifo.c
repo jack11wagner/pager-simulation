87b7915ac5504f332e37afaa8fa68c7ee88e54d7
Riley Kirkpatrick <kirkpatrickr@moravian.edu>
2019-11-13 23:17:25 -0500

    Simplify FIFO

--- fifo.c
+++ fifo.c
@@ -11,11 +11,6 @@
 // use based on the FIFO algorithm. It returns the frame number of the selected frame but does not
 // update any pager data.
 uint64 fifo_select_victim_frame(pager_data* pager) {
-	// Select the first empty frame available, if any
-	if (pager->num_free_frames > 0) { return pager->num_frames - pager->num_free_frames; }
-	
-	// Remove next victim, add a new victim, and return the next victim (all from/into the FIFO queue)
-	uint64 victim_frame = pager->FIFO_victim;
-	pager->FIFO_victim = (pager->FIFO_victim + 1) % pager->num_frames;
-	return victim_frame;
+	// Return the victim
+	return (pager->FIFO_victim = (pager->FIFO_victim + 1) % pager->num_frames);
 }
